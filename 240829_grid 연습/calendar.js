a847ef92ee90561ae7d461bbf8b3717fa1646cd0
왕현주 <w2324@e-mirim.hs.kr>
2024-09-23 15:03:09 +0900

    css grid 연습 calendar js changeMonth()

--- 240829_grid 연습/calendar.js
+++ 240829_grid 연습/calendar.js
@@ -1,23 +1,41 @@
-//이전 / 다음 버튼 클릭하면 이전달/다음달로 변경하자
+
 
 //현재 날짜 구하자
 currentDate = new Date();
-//년 구하자
-const year = currentDate.getFullYear();
 
-//월 구하자
-const month =currentDate.getMonth(); //1월 : 0
 
-//제목 표시하자
 //HTML -> js 변수 가져오자 #calendar-header > h1
 const calendarHeader = document.getElementById("calendar-header");
 const calendarHeaderH1 = calendarHeader.getElementsByTagName("h1")[0];
 // const calendarHeaderH1 = document.querySelector("#calendar-header h1");
 
 
-//js 벼누에 innerHtml = `$(year)년 $(month + 1)월`
-calendarHeaderH1.innerHTML = `<i>${year}년 ${month + 1}월</i>`;
 
-console.log(`$(year)년 $(month + 1)월`);
+//이전/다음 버튼 클릭했을 때 이전달/다음달로 변경하자
+//HTML -> js 변수
+//click event 발생 했을 때 , 해야 할 일 정하자
 
-prev-month
+const prevMonthButton = document.getElementById("prev-month");
+// prevMonthButton.addEventListener("cilck",console.log("이전")); //리턴값이 undefined => 클릭했을때 가만히 있으라
+prevMonthButton.addEventListener("click", () => changeMonth(-1)); //중요하다.매우중요하다.
+
+
+const nextMonthButton = document.querySelector("#next-month");
+nextMonthButton.onclick = () => changeMonth(1);
+
+//diff: -1: 이전 달, 0: 현재 달, 1: 다음 달
+const changeMonth = (diff) => {
+    currentDate.setMonth(currentDate.getMonth() + diff);
+    //년 구하자
+    const year = currentDate.getFullYear();
+
+    //월 구하자
+    const month = currentDate.getMonth(); //1월 : 0
+    //제목 바꾸자
+    // console.log(`${year}년 ${month + 1}월`);
+
+    //js 벼누에 innerHtml = `$(year)년 $(month + 1)월`
+    calendarHeaderH1.innerHTML = `<i>${year}년 ${month + 1}월</i>`;
+}
+
+changeMonth(0); //현재 달 출력
